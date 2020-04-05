f4bc43f17ed104d399a3993e0e10612da511c25b
Alexis Jouin <alexis.jouin@live.fr>
2020-04-05 11:03:44 +0200

    ADD : Traitement connection ADD : Session connection TODO : gérer la déconnection

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
   <body>
     <?php
       include("/includes/menu.php");
+      session_start();
+      $name = $_SESSION['name'];
+      $email = $_SESSION['email'];
     ?>
     <h1>Accueil</h1>
+    <h2>Bienvenue <?php echo $name; ?></h2>
+    <p><?php echo $email; ?></p>
 
   </body>
 
---
+++ traitement/connection-traitement.php
@@ -0,0 +1,54 @@
+<?php
+  $user = "root";
+  $pass = "";
+
+  $email=$_POST['email'];
+  $pwd=$_POST['pwd'];
+
+  try {
+      //Connection à la BDD
+      $dbh = new PDO('mysql:host=localhost;dbname=eCommerce', $user, $pass);
+
+      //On check si le user existe et le pwd match bien
+      $stmt1 = $dbh->prepare('SELECT email, pwd, name FROM user WHERE email LIKE :email');
+      $stmt1->bindParam(':email', $email);
+      $stmt1->execute();
+      $result = $stmt1->fetch();
+
+      $resEmail = $result[0];
+      $resPwd = $result[1];
+      $resName = $result[2];
+
+      if($resEmail == $email){
+        if($resPwd == $pwd){
+          //Si le mot de passe est correcte alors on redirige sur la page d'accueil
+          //On démarre la session
+          session_start();
+          //Création des variables session
+          $_SESSION['name'] = $resName;
+          $_SESSION['email'] = $resEmail;
+          //Redirection vers accueil
+          print "Connection réussi ! Redirection ... ";
+          echo "<script>alert('Connection réussi ! Redirection ...');</script>";
+          echo "<script>setTimeout(\"location.href = '../index.php';\",1500);</script>";
+        }
+        else{
+          print "Mot de passe incorrecte ! Redirection ... ";
+          echo "<script>alert('Mot de passe incorrecte ! Redirection ... ');</script>";
+          echo "<script>setTimeout(\"location.href = '../connection.php';\",1500);</script>";
+        }
+      }
+      else{
+        print "Utilisateur inconnus ! Redirection ... ";
+        echo "<script>alert('Utilisateur inconnus ! Redirection ... ');</script>";
+        echo "<script>setTimeout(\"location.href = '../connection.php';\",1500);</script>";
+      }
+
+      $dbh = null;
+  } catch (PDOException $e) {
+      print "Erreur !: " . $e->getMessage() . "<br/>";
+      die();
+  }
+
+
+?>
