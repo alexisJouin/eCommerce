a0994185d55f6022cfe9011934ec575a4c66ae40
Alexis Jouin <alexis.jouin@live.fr>
2020-04-20 12:49:20 +0200

    ADD : 4 articles aléatoires pour la page d'accueil (connecté)

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   <head>
     <title>Accueil - eCommerce</title>
     <?php include("/includes/include-files.php");  ?>
+    <?php include("/lib/fonctions.php");  ?>
   </head>
 
   <body>
@@ -21,8 +22,51 @@
     ?>
     <h1>Accueil</h1>
     <h2>Bienvenue <?php echo $name; ?></h2>
-    <p><?php echo $email; ?></p>
 
+    <?php
+      if (isset($_SESSION['email']) && isset($_SESSION['name']) ) {
+     ?>
+    <p>Voici quelques articles qui pourraient vous intéresser :</p>
+    <div class="container-articles">
+      <?php
+      //Params BDD
+      $user = "root";
+      $pass = "";
+
+      try {
+          //Connection à la BDD
+          $dbh = new PDO('mysql:host=localhost;dbname=eCommerce', $user, $pass);
+
+          //On selectionne aléatoirement 4 articles
+          $stmt1 = $dbh->prepare('SELECT a.name, photo, author, price, u.name AS authorName
+                                  FROM article a
+                                  INNER JOIN user u
+                                  WHERE u.id = a.author
+                                  ORDER BY RAND()
+                                  LIMIT 4');
+
+          $stmt1->execute();
+          $result = $stmt1->fetchAll();
+
+          foreach($result as $article){
+            $photoPath = skip_accents(str_replace(' ', '', $article['photo']));
+            print_r("<div class='div-article'>");
+            print_r("<h2>" . $article['name']. "</h2>");
+            print_r("<img class='img-article' src='photos/". $photoPath . "'>" );
+            print_r("</br>");
+            print_r("<h3>" . $article['price']. "€</h3>");
+            print_r("<h4>Vendu par " . $article['authorName']. "</h4>");
+            print_r("</div>");
+          }
+
+
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+       ?>
+     </div>
+   <?php } ?>
   </body>
 
 
